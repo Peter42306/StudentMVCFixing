13694b7bdf553d05063d3e8ef56acd4dec5980cb
Petr Zalizko <pzalizko@gmail.com>
2024-07-19 16:34:20 +0300

    Add simplest MVC with database

---
+++ StudentMVCFixing/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentMVCFixing.Models;
+
+namespace StudentMVCFixing.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentContext _studentContext;
+
+        public StudentController(StudentContext studentContext)
+        {
+            _studentContext = studentContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Student>students=await _studentContext.Students.ToListAsync();
+            ViewBag.Students = students;
+            return View();
+        }        
+    }
+}
